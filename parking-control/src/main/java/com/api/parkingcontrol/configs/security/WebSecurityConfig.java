8d84712fb6d3975d6113f0c320e5fa6fb5f8a9e8
afernandes97 <andreluissilvafernandes@gmail.com>
2022-08-30 16:58:02 -0300

    atualiza webSecurityConfig, desabilita csrf

--- parking-control/src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
+++ parking-control/src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+//existe uma notação chamada @EnableWebSecurity que é muito utilizada, quando ela esta implementada a mesma "desliga" todas as configurações default do spring, e assim ira levar as configurações personalizadas que fizmos aqui na classe.
+//@EnableWebSecuriy
 //avisando ao spring que essa classe sera um Bean de configuração
 @Configuration
 //sera abordado duas maneiras, a primeira seria a forma "tradicional" utilizada anteriormente, muitos projetos caso nao feito atualização, sera encontrado dessa maneira, utilizando a classe websecurityconfigureradapter
@@ -24,6 +26,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { //a classe
                 //anyrequest permitall avisando q sera permitido para qlr acesso(ainda nao expecificando as requisiçoes e que posteriormente sera definido)
                 //.anyRequest().permitAll();
                 //definindo que qualquer solicitação a api para consumo deve estar autenticado.
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                //sempre que quiser unir as configurações utilizar and
+                .and()
+                //Desabilitando o padrão do spring que tras habilitado o csrf
+                .csrf().disable();
+
+        //por padrão de segurança para evitar Cross Site Request Forgery o spring security trás uma configuração default para qualquer metodo construtivo ou destrutivo ele já tras o CSRF ativo.
     }
 }
